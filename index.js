3624a854b23627ee9db2521c26857130cf001ab7
rosnifarook <rosnifarook24@gmail.com>
2025-09-29 14:58:59 +0530

    Fixed index.js issues

--- index.js
+++ index.js
@@ -24,14 +24,17 @@ app.use("/api/orders", orderRoutes)
 app.use("/api/auth", userRoutes)
 app.use("/api/admin", adminRoutes)
 
+app.get("/", (req, res) => {
+    res.send("Book Store Server is running!");
+});
+
 async function main() {
   await mongoose.connect(process.env.DB_URL);
-  app.get("/", (req, res) => {
-    res.send("Book Store Server is running!");
-  });
 }
 
-main().then(() => console.log("Mongodb connect successfully!")).catch(err => console.log(err));
+main()
+  .then(() => console.log("Mongodb connect successfully!"))
+  .catch(err => console.log(err));
 
 // app.listen(port, () => {
 //   console.log(`Example app listening on port ${port}`);
