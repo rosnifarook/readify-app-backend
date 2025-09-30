dd7b08dbe9893303022f5388024636029801f002
rosnifarook <rosnifarook24@gmail.com>
2025-10-01 00:14:37 +0530

    Updated .env file

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require('dotenv').config()
 // middleware
 app.use(express.json());
 app.use(cors({
-    origin: ['https://readify-app-frontend.vercel.app','http://localhost:5173'],
+    origin: [process.env.FRONTEND_URL,'http://localhost:5173'],
     credentials: true
 }))
 
